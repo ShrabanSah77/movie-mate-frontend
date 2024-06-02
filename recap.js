4c4290bb90e9f445cce95a2283ae296765b01870
ShrabanSah77 <shrabanshah77@gmail.com>
2024-06-02 16:07:42 +0545

    changes

--- recap.js
+++ recap.js
@@ -35,3 +35,35 @@ console.log("Hello world");
 // ANS: the process whereby the interpreter appears to move
 // the declaration of functions, variables, classes, or imports to
 // the top of their scope, prior to execution of the code.
+
+// 1. Statement.
+
+// if ((gender = "m")) {
+//   console.log("You are Male");
+// } else {
+//   console.log("you are female");
+// }
+
+// 3 or more
+
+/*
+switch(){
+    case:
+    default:
+}
+*/
+
+const gen = "o";
+switch (true) {
+  case gen === "m":
+    console.log("you are male");
+    break;
+  case gen === "f":
+    console.log("you are female");
+    break;
+  case gen === "o":
+    console.log("you are third gender");
+    break;
+  default:
+    console.log("something went wrong");
+}
