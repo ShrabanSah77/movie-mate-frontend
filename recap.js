c77bc3fa746d75af1afd1385749d561224592155
ShrabanSah77 <shrabanshah77@gmail.com>
2024-06-03 09:05:40 +0545

    JS recap

--- recap.js
+++ recap.js
@@ -84,22 +84,22 @@ switch(){
 
 // write the fibonacci series for nth Number
 
-function fib(n) {
-  let a = 0,
-    b = 1,
-    c,
-    i;
-  if (n == 0) return a;
-  for (i = 2; i <= n; i++) {
-    c = a + b;
-    a = b;
-    b = c;
-  }
-  return b;
-}
+// function fib(n) {
+//   let a = 0,
+//     b = 1,
+//     c,
+//     i;
+//   if (n == 0) return a;
+//   for (i = 2; i <= n; i++) {
+//     c = a + b;
+//     a = b;
+//     b = c;
+//   }
+//   return b;
+// }
+// let n = 9;
+// document.write(fib(n));
 
 // Driver code
 
-let n = 9;
 
-document.write(fib(n));
