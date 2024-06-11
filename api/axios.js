854a2fd78d3a67aa71044deed129128dcf5b7233
ShrabanSah77 <shrabanshah77@gmail.com>
2024-06-11 14:17:07 +0545

    update axios.

--- api/axios.js
+++ api/axios.js
@@ -1,3 +1,31 @@
-const axios = require("axios");
+// const axios = require("axios");
 
-const getProducts = async
+// const getProducts = async (url) => {
+//   try {
+//     const { data } = await axios.get(url);
+//     console.log({ data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// getProducts("https://dummyjson.com/products");
+
+const axios = require("axios");
+const instance = axios.create({
+  baseURL: "https://dummyjson.com",
+  timeOut: 5000,
+});
+
+const getProducts = async (url) => {
+  try {
+    const controller = new AbortController();
+    const { data } = await instance.get(url, { signal: controller.signal }); // URL
+    console.log(data);
+    controller.abort();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// List the product name in the Home page (index.html)
--- api/index.js
+++ api/index.js
@@ -1,6 +1,31 @@
-// cosnt randomTalk
+// const randomTalk = {
+// // Fetch API
 
-const getProducts = (url) => {};
+// // Fetch data from this API
 
+// // Axios
 
-getProducts()
+// // Fetch data from this API
+// // https://dummyjson.com/products
+
+// // Advance
+// // Axios Instance
+// // TimeOut
+// // Abort Controller
+// }
+
+const getProducts = async (url) => {
+  //   fetch(url) (// promise chainig)
+  //     .then((d) => d.json()) // Fetch required
+  //     .then((result) => console.log({ result }));
+
+  try {
+    const d = await fetch(url);
+    const result = await d.json();
+    console.log({ result });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getProducts("https://dummyjson.com/products");
